cc90fadd1de8217de52175b0293c0f65dd99813f
Jhonathan Martinez <ing.jhonathan.martines@gmail.com>
2026-05-23 10:47:34 -0400

    infoemp

--- server.js
+++ server.js
@@ -199,6 +199,9 @@ app.post('/api/employees/delete-multiple', verificarToken, (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('🚀 Servidor corriendo en http://192.168.11.211:3000');
+// Busca tu app.listen y cámbiala para que se vea exactamente así:
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor global corriendo en el puerto ${PORT}`);
 });
